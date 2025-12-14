478e1eee4a4bea731100f2ade9035e2055bc09af
Akshata Umagol <88843840+akshataumagol@users.noreply.github.com>
2025-12-15 00:03:26 +0530

    Update PayPalButton.jsx

--- view/src/components/Cart/PayPalButton.jsx
+++ view/src/components/Cart/PayPalButton.jsx
@@ -1,40 +1,4 @@
-/*import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-const PayPalButton = ({ amount, onSuccess, onError }) => {
-  return (
-    <PayPalScriptProvider
-      options={{
-        "client-id": "AcsuQ_HNYwe4LQnqMsCVWz5-qkIEEBu-T8_pQu1A0pTrVOwszRy2USrOkGoJjVNKxNhZJZhiE-vaUV9l",
-      }}
-    >
-      <PayPalButtons
-        style={{ layout: "vertical" }}
-        createOrder={(data, actions) => {
-          return actions.order.create({
-            purchase_units: [
-              {
-                amount: {
-                  value:amount
-                }
-              }
-            ],
-          });
-        }}
-        onApprove={(data, actions) => {
-          return actions.order.capture().then((details) => {
-            onSuccess(details);
-          });
-        }}
-        onError={(err) => {
-          onError(err);
-        }}
-      />
-    </PayPalScriptProvider>
-  );
-};
-
-export default PayPalButton;
-*/
 import React from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
