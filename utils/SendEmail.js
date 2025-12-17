2f0e3da81df594448ce336c2ae3dc058ae2550f3
Akshata Umagol <88843840+akshataumagol@users.noreply.github.com>
2025-12-17 13:04:29 +0530

    Create SendEmail.js

---
+++ utils/SendEmail.js
@@ -0,0 +1,22 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "9e2947001@smtp-brevo.com", // your Brevo login
+    pass: process.env.BREVO_SMTP_KEY,
+  },
+});
+
+const sendEmail = async ({ to, subject, html }) => {
+  await transporter.sendMail({
+    from: '"ShopIt" <akshumagol2000@gmail.com>', 
+    to,
+    subject,
+    html,
+  });
+};
+
+module.exports = sendEmail;
