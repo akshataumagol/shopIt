28490173f0b38a2a7cae22aa9de942eb6fbcafcf
Akshata Umagol <88843840+akshataumagol@users.noreply.github.com>
2025-12-23 14:25:27 +0530

    Update main.jsx

--- view/src/main.jsx
+++ view/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+/*import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,4 +7,21 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+)*/
+// src/main.jsx or index.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
+
