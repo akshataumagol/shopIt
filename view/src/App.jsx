c8fec8ff8d87db0736be47ee1ff88d571d12c298
Akshata Umagol <88843840+akshataumagol@users.noreply.github.com>
2025-12-23 13:24:39 +0530

    Update App.jsx

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import UserLayout from "./components/Layout/UserLayout";
@@ -45,6 +45,66 @@ function App() {
               path="collection/:category/:subCategory"
               element={<CollectionPage />}
             />
+            <Route path="/products/:id" element={<ProductDetails />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
+  );
+}
+
+export default App;*/
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./context/CartContext";
+import UserLayout from "./components/Layout/UserLayout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import TrackOrder from "./pages/TrackOrder";
+import Checkout from "./components/Cart/Checkout";
+import OrderConfirmation from "./pages/OrderConfirmationPage";
+import CollectionPage from "./pages/CollectionPage";
+import ProtectedRoute from "../../routes/ProtectedRoute";
+import ProductDetails from "./components/Products/ProductDetails";
+import { Toaster } from "sonner";
+
+function App() {
+  const [user, setUser] = useState(null);
+  
+  return (
+    <CartProvider>
+      <BrowserRouter>
+        <Toaster position="top-right" />
+        <Routes>
+          <Route path="/" element={<UserLayout />}>
+            <Route index element={<Home user={user} />} />
+            <Route path="login" element={<Login setUser={setUser} />} />
+            <Route path="register" element={<Register setUser={setUser} />} />
+            <Route path="profile" element={<Profile user={user} />} />
+            <Route path="/track-order/:orderId" element={<TrackOrder />} />
+            
+            {/* Protected Checkout Route */}
+            <Route
+              path="checkout"
+              element={
+                <ProtectedRoute>
+                  <Checkout user={user} />
+                </ProtectedRoute>
+              }
+            />
+            
+            <Route
+              path="order-confirmation/:orderId"
+              element={<OrderConfirmation />}
+            />
+            
+            <Route
+              path="collection/:category/:subCategory"
+              element={<CollectionPage />}
+            />
+            
             <Route path="/products/:id" element={<ProductDetails />} />
           </Route>
         </Routes>
