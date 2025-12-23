503b0fb1952d0a13c42550a5ce31ed7c412efd85
Akshata Umagol <88843840+akshataumagol@users.noreply.github.com>
2025-12-23 15:06:08 +0530

    Update ProtectedRoute.jsx

--- view/src/components/Common/ProtectedRoute.jsx
+++ view/src/components/Common/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 // src/routes/ProtectedRoute.jsx
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
