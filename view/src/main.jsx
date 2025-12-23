f8bc72ef7d7b153586dbea63368e25eab24b17b4
Akshata Umagol <88843840+akshataumagol@users.noreply.github.com>
2025-12-23 14:38:31 +0530

    Update main.jsx

--- view/src/main.jsx
+++ view/src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById('root')).render(
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css'
-import App from "./App";
+import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import { CartProvider } from "./context/CartContext";
 
